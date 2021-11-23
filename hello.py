d1aed12627961d6455307c371a388411051feecf
David Chicas <dchicasduena@mun.ca>
2021-11-23 14:33:53 -0330

    my first commit

---
+++ hello.py
@@ -0,0 +1,7 @@
+
+# This is the program you will be saving in your repository. 
+# It is a simple program that will print some information
+
+name = "your_name" # change this to your name, but do not forget the " "
+
+print("\nHello! my name is " + name + ". \nI am learning how to use git!\n")
